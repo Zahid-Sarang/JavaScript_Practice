68fc3c2423ebae58433ba7369a3c9d92bbed791e
Zahid-Sarang <88975408+Zahid-Sarang@users.noreply.github.com>
2023-02-24 16:20:06 +0000

    PROTOTYPE DONE

--- tutorial/OOP/prototype/prototype.js
+++ tutorial/OOP/prototype/prototype.js
@@ -1,6 +1,8 @@
 // problem => every time we create object method also repeat which take extra memory
 // solution => shared a method using prototype
 
+// ===================================================================================================================================//
+
 function BankAccount(customerName, balance = 0) {
   this.customerName = customerName;
   this.balance = balance;
@@ -21,23 +23,38 @@ function BankAccount(customerName, balance = 0) {
     in the javaScript, function also a object 
 */
 
+// ===================================================================================================================================//
+
 // in the javaScript whenever function is created a prototype property add to every function
 // and prototype value is empty object {}
 
-// BankAccount.prototype.deposit = function (amount) {
-//   // you can not use arrow function because arrow func doesn't have it's own context and it will be find this.balnce on window object and it dot get it
-//   this.balance += amount;
-// };
+// =============================== deposite ============================================================================ //
 
-// console.log(BankAccount.prototype);
+BankAccount.prototype.deposit = function (amount) {
+  // you can not use arrow function because arrow func doesn't have it's own context and it will be find this.balnce on window object and it dot get it
+  this.balance += amount;
+};
 
-BankAccount.prototype.withdraw = function (amount) {
-  this.balance -= amount
-}
+console.log(BankAccount.prototype);
 
-const account1 = new BankAccount("John", 10000);
-// account1.deposit(500);
- account1.withdraw (400)
+const account1 = new BankAccount("John", 100);
+account1.deposit(500);
 console.log(account1);
 
 // Javascript will first check in the constructor, if it does not find the method, then it will check in the prototype. if it still dont get that then it will check BankAccount constructor
+
+// ===================================================================================================================================//
+
+// =============================== withdraw ============================================================================ //
+
+BankAccount.prototype.withdraw = function (amount) {
+  this.balance -= amount;
+};
+
+console.log(BankAccount.prototype);
+
+const account2 = new BankAccount("Jane Doe", 2000);
+account2.withdraw(500);
+console.log(account2);
+
+// ===================================================================================================================================//
