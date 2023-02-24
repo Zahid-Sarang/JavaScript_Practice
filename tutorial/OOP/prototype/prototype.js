938002c219f16ad8839d2160bf7261ed806023f5
Zahid-Sarang <88975408+Zahid-Sarang@users.noreply.github.com>
2023-02-24 16:12:12 +0000

    prototye deposit method

--- tutorial/OOP/prototype/prototype.js
+++ tutorial/OOP/prototype/prototype.js
@@ -1,4 +1,4 @@
-// problem => every time we create object method also repeat which take extra memory 
+// problem => every time we create object method also repeat which take extra memory
 // solution => shared a method using prototype
 
 function BankAccount(customerName, balance = 0) {
@@ -6,23 +6,33 @@ function BankAccount(customerName, balance = 0) {
   this.balance = balance;
   this.accountNumber = Date.now();
 
-  this.deposit = function (amount){
-    this.balance += amount;
-  };
-  this.withdraw = (amount)=>{
+  // this.deposit = function (amount) {
+  //   this.balance += amount;
+  // };
+  this.withdraw = (amount) => {
     this.balance -= amount;
-  }
+  };
 }
-
-//   const account1 = new BankAccount('John', 100);
-//   console.log(account1)
 
 //   const account2 = new BankAccount('Jane Doe', 200);
 //   console.log(account2)
 
-
 /*
-  in the javaScript, function also a object 
+    in the javaScript, function also a object 
 */
 
+// in the javaScript whenever function is created a prototype property add to every function
+// and prototype value is empty object {}
+
+BankAccount.prototype.deposit = function (amount) {
+  // you can not use arrow function because arrow func doesn't have it's own context and it will be find this.balnce on window object and it dot get it
+  this.balance += amount;
+};
+
 console.log(BankAccount.prototype);
+
+const account1 = new BankAccount("John", 100);
+account1.deposit(500);
+console.log(account1);
+
+// Javascript will first check in the constructor, if it does not find the method, then it will check in the prototype. if it still dont get that then it will check BankAccount constructor
