750d3a52abfcda1f41f4fa2d033526786ffbb840
Zahid-Sarang <88975408+Zahid-Sarang@users.noreply.github.com>
2023-02-25 10:47:45 +0000

    classes done

--- tutorial/OOP/classes/classes.js
+++ tutorial/OOP/classes/classes.js
@@ -3,14 +3,29 @@ class BankAccount {
     accountNumber;
     blance = 0;
 
-    constructor(customerName,blance){
-        this.accountNumber= Date.now()
+    constructor(customerName, blance) {
+        this.accountNumber = Date.now()
         this.customerName = customerName;
         this.blance = blance;
     }
+
+    deposit(amount) {
+        this.blance += amount
+    }
+
+    withdraw(amount) {
+        this.blance -= amount
+    }
 }
 
-const John_Doe = new BankAccount ("Jhon Doe",2000) ;
+const John_Doe = new BankAccount("Jhon Doe", 2000);
+
+John_Doe.deposit(1000)
 
 console.log(John_Doe)
 
+const Harry = new BankAccount("Harry", 5000)
+
+Harry.withdraw(3000);
+
+console.log(Harry)
