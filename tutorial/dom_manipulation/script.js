14e59d8a755d6d1747b3d65a9af5c683c2d9302f
zahid-soft <zahidsarang95@gmail.com>
2023-03-22 16:19:07 +0530

    list out the all method for selecting the elements

---
+++ tutorial/dom_manipulation/script.js
@@ -0,0 +1,32 @@
+/*------------------------------*/
+/*       Dom manipulation       */
+/*------------------------------*/
+
+// 1. getElementById
+
+// const heading = document.getElementById("heading")
+// console.log(heading)
+
+// 2. getElementsByTagName
+
+// const headings = document.getElementsByTagName("h1");
+// console.log(headings[0])
+
+//3. getElementByClassName
+
+// const heading2 = document.getElementsByClassName("heading")
+// console.log(heading2)
+
+// 4. querySelector
+
+// const heading = document.querySelector('.heading')
+// const headings = document.querySelector('#heading')
+// console.log(heading)
+// console.log(headings)
+
+
+// 5. querySelectorAll
+
+// const heading = document.querySelectorAll('.heading');
+// console.log(heading)
+
